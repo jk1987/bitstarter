6afe1c64ff19bc21d2c892f124ce0170713d9e20
jk1987 <sanscfc@gmail.com>
2013-07-14 15:37:15 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env mode
-
 var express = require('express');
-var fs  = require("fs");
+
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync('/index.html', 'utf8')
-console.log(text);
-
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var buffer = new Buffer();
+    response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
