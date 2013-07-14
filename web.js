679458394c0c3018e09bbfd5fab5414bfc2ebb31
jk1987 <sanscfc@gmail.com>
2013-07-14 20:30:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,13 +4,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    mybuf = readFileSync("./index.html");
-    var buf = new Buffer(mybuf);
-    response.send(buf.toString('utc-8'));
-};
+    var mybuf = new Buffer(readFileSync("index.html"));
+    response.send(mybuf.toString('utc-8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-};
+});
 	   
