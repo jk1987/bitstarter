23db71c4db187d556a9fbb324c554dc13b4b3602
jk1987 <sanscfc@gmail.com>
2013-07-14 15:12:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var fs  = require("fs");
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync('./index.html', 'utf8')
+var text = fs.readFileSync('/index.html', 'utf8')
 console.log(text);
 
 
